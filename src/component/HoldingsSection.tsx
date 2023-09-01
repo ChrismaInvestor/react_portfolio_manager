65f3cb1229719538e5b6560da832e2cd0d98030d
Chris Ma <snake1950@163.com>
2023-09-01 18:33:46 +0800

    20230901 day job

--- src/component/HoldingsSection.tsx
+++ src/component/HoldingsSection.tsx
@@ -4,6 +4,8 @@ import SingleSelect from "./SingleSelect";
 import React from "react";
 import { listPortfolios } from "../api/ListPortfolios";
 import { Portfolio } from "./AddPortfolioSection";
+import { useQuery } from "@tanstack/react-query";
+import { BASE_URL } from "../constant/Constant";
 
 const columns: GridColDef[] = [
   { field: "code", headerName: "证券代码", width: 100 },
@@ -15,6 +17,7 @@ const columns: GridColDef[] = [
 
 export default function HoldingsSection() {
   const [portfolioOptions, setPortfolioOptions] = React.useState<string[]>([]);
+  const [currentPortfolio, setCurrentPortfolio] = React.useState<string>();
 
   React.useEffect(() => {
     listPortfolios().then((portfolios) => {
@@ -25,22 +28,35 @@ export default function HoldingsSection() {
     });
   }, []);
 
+  const { isLoading, data } = useQuery(["getDynamics", currentPortfolio], () =>
+    fetch(
+      BASE_URL + "portfolio/dynamics?currentPortfolio=" + currentPortfolio
+    ).then((res) => res.json())
+  );
+
+  console.log(data);
+
   return (
     <Grid container spacing={2} sx={{ p: 2 }}>
       <Grid item xs={12}>
-        <SingleSelect placeholder="选择投资组合" options={portfolioOptions} />
+        <SingleSelect
+          placeholder="选择投资组合"
+          options={portfolioOptions}
+          setValue={setCurrentPortfolio}
+          value={currentPortfolio}
+        />
       </Grid>
       <Grid item xs={6}>
-        当前现金:{" "}
+        当前现金:{data && data.cash}
       </Grid>
       <Grid item xs={6}>
-        当前证券市值:{" "}
+        当前证券市值:{data && data.securityMarketValue}
       </Grid>
       <Grid item xs={6}>
-        总市值:{" "}
+        总市值:{data && data.totalMarketValue}
       </Grid>
       <Grid item xs={6}>
-        收益率:{" "}
+        收益率:{data && data.profitMargin}
       </Grid>
       <Grid item xs={12}>
         <DataGrid columns={columns} rows={[]} />
--- src/component/SingleSelect.tsx
+++ src/component/SingleSelect.tsx
@@ -1,16 +1,38 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 type Props = {
   placeholder: string;
   options: string[];
+  value: string | undefined;
+  setValue?: (e: any) => void;
+  name?: string;
+  disabled?: boolean;
 };
 
 export default function SingleSelect(props: Props) {
-  const { placeholder, options } = props;
+  const { placeholder, options, value, setValue, name, disabled } = props;
+
+  const handleChange = (event: SelectChangeEvent) => {
+    !!setValue && setValue(event.target.value);
+  };
   return (
     <FormControl sx={{ width: "100%" }} fullWidth>
       <InputLabel id="select-label">{placeholder}</InputLabel>
-      <Select labelId="select-label" id="single-select" label={placeholder}>
+      <Select
+        labelId="select-label"
+        id="single-select"
+        label={placeholder}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        disabled={!!disabled && disabled}
+      >
         {options.map((option) => (
           <MenuItem key={option} value={option}>
             {option}
