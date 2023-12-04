be50b1b57afbb0961657e63a5920418ca3ee2ddd
Chris Ma <snake1950@163.com>
2023-12-04 23:10:11 +0800

    20231204 night job

--- src/pages/DataValidationPage.tsx
+++ src/pages/DataValidationPage.tsx
@@ -1,21 +1,18 @@
 import {
   Button,
   Card,
-  CardContent,
   CircularProgress,
   Grid,
   Typography,
 } from "@mui/material";
 import { BASE_URL } from "../constant/Constant";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
 
 export default function DataValidationPage() {
   const [stocksUpdateStatus, setStocksUpdateStatus] =
     React.useState<string>("股票列表更新");
 
-  const [minPriceUpdateStatus, setMinPriceUpdateStatus] =
-    React.useState<string>("分钟级价格更新");
   const mutation = useMutation({
     mutationFn: () => {
       return fetch(BASE_URL + "data/stockList", {
@@ -27,23 +24,8 @@ export default function DataValidationPage() {
     },
   });
 
-  const mutation2 = useMutation({
-    mutationFn: () => {
-      return fetch(BASE_URL + "data/minPrice", {
-        method: "GET",
-      });
-    },
-    onSuccess: (data) => {
-      data.json().then((data) => setMinPriceUpdateStatus(data.data));
-    },
-  });
-
   const handleStockUpdateClick = () => {
     mutation.mutate();
-  };
-
-  const handleMinPriceUpdateClick = () => {
-    mutation2.mutate();
   };
 
   return (
@@ -64,31 +46,6 @@ export default function DataValidationPage() {
             ) : (
               <Typography variant="h5" alignContent={"center"}>
                 {stocksUpdateStatus}
-              </Typography>
-            )}
-          </Card>
-        </Button>
-      </Grid>
-      <Grid item xs={6}>
-        <Button
-          sx={{ width: "100%", height: 200 }}
-          onClick={handleMinPriceUpdateClick}
-        >
-          <Card
-            sx={{
-              width: "100%",
-              height: "100%",
-              backgroundColor: "lightYellow",
-            }}
-          >
-            <Typography variant="h4" alignContent={"center"}>
-              分钟级价格更新
-            </Typography>
-            {mutation2.isLoading ? (
-              <CircularProgress />
-            ) : (
-              <Typography variant="h5" alignContent={"center"}>
-                {minPriceUpdateStatus}
               </Typography>
             )}
           </Card>
