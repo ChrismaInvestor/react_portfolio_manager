6e9851bd1fdc89292454baec4fffcae046d0c2ae
Chris Ma <snake1950@163.com>
2023-12-05 23:40:12 +0800

    2023-12-05 night job

--- src/component/PreorderDialog.tsx
+++ src/component/PreorderDialog.tsx
@@ -3,6 +3,7 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  Grid,
   Stack,
   Typography,
 } from "@mui/material";
@@ -11,11 +12,14 @@ import { OrderPlacementContext } from "../context/OrderPlacementContext";
 import React from "react";
 import { useMutation } from "@tanstack/react-query";
 import { BASE_URL } from "../constant/Constant";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { Dayjs } from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 type Props = {
   open: boolean;
   data: any[];
-  handleClose: ()=>void;
+  handleClose: () => void;
 };
 
 const columns: GridColDef[] = [
@@ -27,9 +31,13 @@ const columns: GridColDef[] = [
 ];
 
 export default function PreorderDialog(props: Props) {
-  const { open, data,handleClose } = props;
+  const { open, data, handleClose } = props;
 
   const { state } = React.useContext(OrderPlacementContext);
+
+  const [startTime, setStartTime] = React.useState<Dayjs | null>(null);
+
+  const [endTime, setEndTime] = React.useState<Dayjs | null>(null);
 
   const mutation = useMutation({
     mutationFn: () => {
@@ -39,29 +47,56 @@ export default function PreorderDialog(props: Props) {
         body: JSON.stringify({
           orders: data,
           portfolio: state?.currentPortfolio,
+          startTime: startTime,
+          endTime: endTime,
         }),
       }).then((res) => res.json());
     },
   });
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>
-        <Stack direction={"row"} spacing={2}>
-          <Typography>试算</Typography>
-          <Button
-            variant="contained"
-            onClick={() => {
-              mutation.mutate();
-            }}
-          >
-            提交
-          </Button>
-        </Stack>
-      </DialogTitle>
-      <DialogContent>
-        <DataGrid columns={columns} rows={data} />
-      </DialogContent>
-    </Dialog>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          <Stack direction={"row"} spacing={2}>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                <Typography>试算</Typography>
+              </Grid>
+              <Grid item xs={5}>
+                <DateTimePicker
+                  label="开始时间"
+                  value={startTime}
+                  onChange={(newValue) => setStartTime(newValue)}
+                  disablePast
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <DateTimePicker
+                  label="结束时间"
+                  value={endTime}
+                  onChange={(newValue) => setEndTime(newValue)}
+                  disablePast
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  onClick={() => {
+                    mutation.mutate();
+                  }}
+                >
+                  提交
+                </Button>
+              </Grid>
+            </Grid>
+          </Stack>
+        </DialogTitle>
+        <DialogContent>
+          <DataGrid columns={columns} rows={data} />
+        </DialogContent>
+      </Dialog>
+    </LocalizationProvider>
   );
 }
